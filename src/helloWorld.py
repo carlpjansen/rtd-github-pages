bab822c33681b31f277538627d3404ee69dc246b
carlpjansen <cpj@emlogic.no>
2022-12-13 16:46:53 +0100

    Finished setting up the rtd for github pages for tutorial https://tech.michaelaltfield.net/2020/07/18/sphinx-rtd-github-pages-1

--- src/helloWorld.py
+++ src/helloWorld.py
@@ -1,1 +1,10 @@
-print('Hello World')
+"""This is a very simple helloWorld python script"""
+def helloWorld(msg)
+    """
+    This is a simple fucntion that prints a message"
+    parameters:
+        msg(str): The string to print
+    """
+    print( msg)
+
+helloWorld('Hello World!')
